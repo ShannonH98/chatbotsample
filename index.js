0f552c8d4637bbdb19ce766477ca5ac36c235e6f
ShannonH98 <shannon_henry1998@hotmail.com>
2020-07-21 19:45:23 -0500

    changed end point

--- index.js
+++ index.js
@@ -33,25 +33,9 @@ const
         }
     });
 
+    
     //Add support for GET requests to the webhook
-    app.get('/webhook', (req, res) => {
-        // Your verify token. Should be a random string.
-        let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
-
-        // Parse the query params
-        let mode = req.query['hub.mode'];
-        let token = req.query['hub.verify_token'];
-        let challenge = req.query['hub.challenge'];
-
-        // Checks the mode and the token sent is correct
-        if(mode === 'subscribe' && token === VERIFY_TOKEN) {
-            
-            // Responds with the challenge token from the request
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-
-        } else {
-            // Responds with the '403 Forbidden' if verify token does not match
-            res.sendStatus(403);
-        }
-    });
+    app.get("/", (req, res) => {
+        res.send("Hi I am a chatbot!!");
+      });
+    
