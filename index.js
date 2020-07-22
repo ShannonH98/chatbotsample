8c5fe189c7c344522a1a1da3c6bf04ab8f03e021
ShannonH98 <shannon_henry1998@hotmail.com>
2020-07-21 19:21:35 -0500

    u

--- index.js
+++ index.js
@@ -1,37 +1,57 @@
-'use strict'
+'use strict';
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
+// Imports dependencies and set ip http server
 
-const app = express()
+const
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express().use(bodyParser.json()); // creates http server
 
-app.set('port', (process.env.PORT || 5000))
+    //Sets server port and logs message on success
+    app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-//Allows us to process the data
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+    //Creates the endpoint for webhook
+    app.post('/webhook', (req, res) => {
+        let body = req.body;
 
-//ROUTES
-app.get('/',function(req, res)
-{
-    res.send("Hello I am a chatbot named Shannon")
-})
+        //Checks if this is an event from a page subscription
+        if(body.object === 'page') {
 
-//Facebook
+            //Iterates over each entry
+            body.entry.forEach(function(entry) {
+                
+                //Gets the message. enttry.messaging is an array but will only ever contain one message, so we will get index 0
+                let webhook_event = entry.messaging[0];
+                console.log(webhook_event);
+            });
 
-app.get('/webhook', function(req, res)
-{
-    if(req.query['hub.verify_token'] === "blondiebytes")
-    {
-        res.send(req.query['hub.challenge'])
-    }
-    res.send("Wrong token")
-})
+            // Returns a '200 OK' response to all requests
+            res.status(200).send('EVENT_RECEIVED');
+        } else {
+            // Returns a '404 Not Found' if event is not from the page subscription
+            res.sendStatus(404);
+        }
+    });
 
-app.listen(app.get('port'), function()
-{
-    console.log("running: port")
-})
+    //Add support for GET requests to the webhook
+    app.get('/webhook', (req, res) => {
+        // Your verify token. Should be a random string.
+        let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
 
-//test
+        // Parse the query params
+        let mode = req.query['hub.mode'];
+        let token = req.query['hub.verify_token'];
+        let challenge = req.query['hub.challenge'];
+
+        // Checks the mode and the token sent is correct
+        if(mode === 'subscribe' && token === VERIFY_TOKEN) {
+            
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with the '403 Forbidden' if verify token does not match
+            res.sendStatus(403);
+        }
+    });
